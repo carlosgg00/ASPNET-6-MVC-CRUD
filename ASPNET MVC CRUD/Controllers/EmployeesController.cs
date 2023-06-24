eb08f48ca9512b9d7b9c1159cffde08bcc297321
carlosgg00 <garciaariel@unah.hn>
2023-06-24 16:19:07 -0600

    Agregar archivos de proyecto.

---
+++ ASPNET MVC CRUD/Controllers/EmployeesController.cs
@@ -0,0 +1,110 @@
+﻿using ASPNET_MVC_CRUD.Data;
+using ASPNET_MVC_CRUD.Models;
+using ASPNET_MVC_CRUD.Models.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPNET_MVC_CRUD.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly MVCDemoDbContext mvcDemoDbContext;
+
+        public EmployeesController(MVCDemoDbContext mvcDemoDbContext)
+        {
+            this.mvcDemoDbContext = mvcDemoDbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        //Ver todos los empleados
+        public async Task<IActionResult> Index()
+        {
+            var employees = await mvcDemoDbContext.Employees.ToListAsync();
+            return View(employees);
+        }
+        [HttpPost]
+        //Agregar nuevo empleado
+        public async Task<IActionResult> Add(AddEmployeeViewModel addEmployeeViewRequest)
+        {
+            var employee = new Employee()
+            {
+                Id = Guid.NewGuid(),
+                Name = addEmployeeViewRequest.Name,
+                Email = addEmployeeViewRequest.Email,
+                Salary = addEmployeeViewRequest.Salary,
+                Department = addEmployeeViewRequest.Department,
+                DateOfBirth = addEmployeeViewRequest.DateOfBirth
+            };
+            //Mandando la entidad creada a la base de datos con EntityFrameword
+            await mvcDemoDbContext.Employees.AddAsync(employee);
+            await mvcDemoDbContext.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        //Ver solo un empleado
+        public async Task<IActionResult> View(Guid id)
+        {
+            var employee = await mvcDemoDbContext.Employees.FirstOrDefaultAsync( x => x.Id == id);
+
+            if (employee != null)
+            {
+                var viewModel = new UpdateEmployeeViewModel()
+                {
+                    Id = employee.Id,
+                    Name = employee.Name,
+                    Email = employee.Email,
+                    Salary = employee.Salary,
+                    Department = employee.Department,
+                    DateOfBirth = employee.DateOfBirth
+                };
+                return await Task.Run(() => View("View", viewModel));
+
+            }
+
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> View(UpdateEmployeeViewModel model)
+        {
+            var employee = await mvcDemoDbContext.Employees.FindAsync(model.Id);
+            if (employee != null)
+            {
+                employee.Name = model.Name;
+                employee.Email = model.Email;
+                employee.Salary = model.Salary;
+                employee.DateOfBirth = model.DateOfBirth;
+                employee.Department = model.Department;
+
+                await mvcDemoDbContext.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(UpdateEmployeeViewModel model)
+        {
+            var employee = await mvcDemoDbContext.Employees.FindAsync(model.Id);
+
+            if (employee != null)
+            {
+                mvcDemoDbContext.Employees.Remove(employee);
+                await mvcDemoDbContext.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+
+        }
+    }
+}
---
+++ ASPNET MVC CRUD/Data/MVCDemoDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ASPNET_MVC_CRUD.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPNET_MVC_CRUD.Data
+{
+    public class MVCDemoDbContext : DbContext
+    {
+        public MVCDemoDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+
+    }
+}
